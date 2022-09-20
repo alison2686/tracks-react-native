10e67c30ed5431f71ef12deac820f43063fe3e65
Alison Lee <alison2686@gmail.com>
2022-09-20 12:05:30 -0700

    store token with AsyncStorage

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,30 +1,34 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import createDataContext from './createDataContext';
 import trackerApi from '../api/tracker'
+
 
 const authReducer = (state, action) => {
     switch (action.type) {
         case 'add_error':
             return { ...state, errorMessage: action.payload }
+        case 'signup':
+            return { errorMessage: '', token: action.payload }
         default:
             return state
     }
 }
 
-const signup = dispatch => {
-    return async ({ email, password }) => {
-        // make api request to sign up with that email and password
+const signup = dispatch => async ({ email, password }) => {
+    // make api request to sign up with that email and password
 
-        // if we sign up, modify our state, and say that we are authenticated
+    // if we sign up, modify our state, and say that we are authenticated
 
-        // if signing up fails, we probably need to reflect an error message somewhere
-        try {
-            const response = await trackerApi.post('/signup', { email, password })
-            console.log(response.data)
-        } catch (err) {
-            dispatch({ type: 'add_error', payload: 'Something went wrong with sign up'})
-        }
+    // if signing up fails, we probably need to reflect an error message somewhere
+    try {
+        const response = await trackerApi.post('/signup', { email, password })
+        await AsyncStorage.setItem('token', response.data.token)
+        dispatch({ type: 'signup', payload: response.data.token })
+    } catch (err) {
+        dispatch({ type: 'add_error', payload: 'Something went wrong with sign up'})
     }
 }
+
 
 const signin = (dispatch) => {
     return ({ email, password }) => {
@@ -43,5 +47,5 @@ const signout = (dispatch) => {
 export const { Provider, Context } = createDataContext(
     authReducer,
     { signin, signup, signout },
-    { isSignedIn: false, errorMessage: '' }
+    { token: null, errorMessage: '' }
 )
