1d6d906d0348901e3cfd185dbc80cbb564f18492
Alison Lee <alison2686@gmail.com>
2022-09-30 12:38:37 -0700

    request location permission

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,10 +1,35 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
-import MapView from 'react-native-maps'
+import { StyleSheet } from 'react-native'
+import MapView, { Polyline } from 'react-native-maps'
 
 
 const Map = () => {
-  return <MapView style={styles.map} />
+  let points = []
+  for (let i = 0; i < 20; i++) {
+    if (i % 2 === 0) {
+    points.push({
+        latitude: 37.78914 + i * 0.001,
+        longitude: -122.43756 + i * 0.001
+    })
+    } else {
+        points.push({
+            latitude: 37.78914 - i * 0.002,
+            longitude: -122.43756 - i * 0.001
+        })        
+    }
+  }
+
+  return <MapView 
+    style={styles.map}
+    initialRegion={{
+        latitude: 37.78914,
+        longitude: -122.43756,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01
+    }} 
+    >
+        <Polyline coordinates={points} />
+    </MapView>
 }
 
 const styles = StyleSheet.create({
