66059854057ddcce9540ce13110076541846c876
Alison Lee <alison2686@gmail.com>
2022-12-08 16:25:08 -0800

    add Start Recording btn

--- src/components/TrackForm.js
+++ src/components/TrackForm.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Input, Button } from 'react-native-elements'
+import Spacer from './Spacer'
 
 const TrackForm = () => {
   return (
     <>
-        <Input />
-        <Button title='Start Recording' />
+        <Spacer>
+            <Input placeholder='Enter Track Name' />
+        </Spacer>
+        <Spacer>
+            <Button title='Start Recording' />
+        </Spacer>
     </>
   )
 }
