462e67f009561b2c41630eec761f7ba2dfe9b5b0
Alison Lee <alison2686@gmail.com>
2022-12-09 11:55:32 -0800

    create new track

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -2,16 +2,19 @@ import axios from 'axios'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const instance = axios.create({
-    baseURL: 'http://d30c-135-180-55-9.ngrok.io'
+    baseURL: 'http://cef4-135-180-55-9.ngrok.io'
 })
 
 instance.interceptors.request.use(
     async (config) => {
+        
         const token = await AsyncStorage.getItem('token')
         if (token) {
             config.headers.Authorization = `Bearer ${token}`
         }
         return config
+
+        
     },
     (err) => {
         return Promise.reject(err)
