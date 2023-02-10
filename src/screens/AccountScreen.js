d58c9575e46ce0b70138d119460e70198e92f7a9
Alison Lee <alison2686@gmail.com>
2023-02-10 11:08:02 -0800

    add text to TrackListScreen

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -1,32 +1,29 @@
-import React, { useContext } from 'react'
-import { View, StyleSheet, Text } from 'react-native'
-import { Button } from 'react-native-elements'
-import { SafeAreaView } from 'react-native-safe-area-context';
-import Spacer from '../components/Spacer'
-import { Context as AuthContext } from '../context/AuthContext'
-import { FontAwesome } from '@expo/vector-icons'
+import React, { useContext } from "react";
+import { View, StyleSheet, Text } from "react-native";
+import { Button } from "react-native-elements";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Spacer from "../components/Spacer";
+import { Context as AuthContext } from "../context/AuthContext";
+import { FontAwesome } from "@expo/vector-icons";
 
 const AccountScreen = () => {
-  const { signout } = useContext(AuthContext)
+  const { signout } = useContext(AuthContext);
 
   return (
-    <SafeAreaView forceInset={{ top: 'always' }}>
-        <Text style={{ fontSize: 40 }}>Account Screen</Text>
-        <Spacer>
-          <Button 
-            title='Sign Out'
-            onPress= {signout}
-          />
-        </Spacer>
+    <SafeAreaView forceInset={{ top: "always" }}>
+      <Text style={{ fontSize: 40 }}>Your Account</Text>
+      <Spacer>
+        <Button title="Sign Out" onPress={signout} />
+      </Spacer>
     </SafeAreaView>
-  )
-}
+  );
+};
 
 AccountScreen.navigationOptions = {
-  title: 'Account',
-  tabBarIcon: <FontAwesome name='gear' size={20} />
-}
+  title: "Account",
+  tabBarIcon: <FontAwesome name="gear" size={20} />,
+};
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
 
-export default AccountScreen
+export default AccountScreen;
--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -1,23 +1,24 @@
-import React, { useContext } from 'react'
-import { StyleSheet, Text, FlatList, TouchableOpacity } from 'react-native'
-import { NavigationEvents } from 'react-navigation'
-import { ListItem } from 'react-native-elements'
-import { Context as TrackContext } from '../context/TrackContext'
+import React, { useContext } from "react";
+import { StyleSheet, Text, FlatList, TouchableOpacity } from "react-native";
+import { NavigationEvents } from "react-navigation";
+import { ListItem } from "react-native-elements";
+import { Context as TrackContext } from "../context/TrackContext";
 
 const TrackListScreen = ({ navigation }) => {
-  const { state, fetchTracks } = useContext(TrackContext)
+  const { state, fetchTracks } = useContext(TrackContext);
 
   return (
     <>
-      <NavigationEvents onWillFocus={fetchTracks}  />
-        <FlatList
+      <NavigationEvents onWillFocus={fetchTracks} />
+      <Text>See your tracks here or go to Add Tracks to start a new one</Text>
+      <FlatList
         data={state}
         keyExtractor={(item) => item._id}
         renderItem={({ item }) => {
           return (
-            <TouchableOpacity 
-              onPress={() => 
-                navigation.navigate('TrackDetail', { _id: item._id })
+            <TouchableOpacity
+              onPress={() =>
+                navigation.navigate("TrackDetail", { _id: item._id })
               }
             >
               <ListItem>
@@ -31,13 +32,13 @@ const TrackListScreen = ({ navigation }) => {
         }}
       />
     </>
-  )
-}
+  );
+};
 
 TrackListScreen.navigationOptions = {
-  title: 'Your Tracks:'
-}
+  title: "Your Tracks:",
+};
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
 
-export default TrackListScreen
+export default TrackListScreen;
